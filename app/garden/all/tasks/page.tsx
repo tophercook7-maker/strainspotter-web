c1a89fb10b084d4aba3db58cdc296193a2fcd6a7
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2025-12-23 13:03:10 -0600

    Fix build: remove headers() call, use only dynamic and revalidate exports

--- app/garden/all/tasks/page.tsx
+++ app/garden/all/tasks/page.tsx
@@ -1,11 +1,7 @@
-import { headers } from "next/headers";
 import TasksClient from "./TasksClient";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
-// Force dynamic rendering by calling headers() at the top level
-headers();
 
 export default function GardenTasksPage() {
   return <TasksClient />;
