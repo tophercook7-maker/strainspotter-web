649d017c1e20c49912410da082c5719866d60078
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2025-12-28 22:26:06 -0600

    Sanitize tokens at localStorage level before Supabase reads them

--- components/FetchSanitizer.tsx
+++ components/FetchSanitizer.tsx
@@ -2,9 +2,13 @@
 
 import { useEffect } from "react";
 import { installFetchSanitizer } from "@/lib/sanitizeFetch";
+import { installSanitizedLocalStorage } from "@/lib/sanitizeLocalStorage";
 
 export default function FetchSanitizer() {
   useEffect(() => {
+    // Install localStorage sanitizer FIRST (before Supabase initializes)
+    installSanitizedLocalStorage();
+    // Then install fetch sanitizer as backup
     installFetchSanitizer();
   }, []);
 
