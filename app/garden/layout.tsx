82f9ae52db4d61e80307ab80f8268b83c63d7319
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2025-12-29 16:38:29 -0600

    Fix empty garden: remove blocking MembershipGate, GardenShell handles membership

--- app/garden/layout.tsx
+++ app/garden/layout.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import AuthWall from "@/components/AuthWall";
-import MembershipGate from "@/components/MembershipGate";
 
 export default function GardenLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthWall>
-      <MembershipGate>
-        <div className="relative" style={{ position: 'relative', zIndex: 1 }}>
-          {children}
-        </div>
-      </MembershipGate>
+      <div className="relative" style={{ position: 'relative', zIndex: 1 }}>
+        {children}
+      </div>
     </AuthWall>
   );
 }
