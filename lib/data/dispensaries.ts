b8d17cafcf18763be7739ec83dfbf56194a23504
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2026-01-10 11:29:41 -0600

    chore: update dispensary dataset

--- lib/data/dispensaries.ts
+++ lib/data/dispensaries.ts
@@ -1,5 +1,16 @@
 export const DISPENSARIES = [
-  { id: "d1", name: "Green Leaf Dispensary", city: "Denver", state: "CO", type: "Recreational" },
-  { id: "d2", name: "Highland Cannabis", city: "Boulder", state: "CO", type: "Medical & Recreational" },
-  { id: "d3", name: "Urban Bloom", city: "Los Angeles", state: "CA", type: "Recreational" },
+  {
+    id: "disp-1",
+    name: "Green Leaf Dispensary",
+    city: "Denver",
+    state: "CO",
+    type: "Recreational",
+  },
+  {
+    id: "disp-2",
+    name: "Sunrise Cannabis",
+    city: "Boulder",
+    state: "CO",
+    type: "Medical",
+  },
 ];
