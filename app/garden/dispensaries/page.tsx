6a385c1700d6ba724ddf9241e913b9cada87ee06
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2026-01-10 09:57:41 -0600

    fix: remove unsupported hours field from dispensary list

--- app/garden/dispensaries/page.tsx
+++ app/garden/dispensaries/page.tsx
@@ -44,9 +44,6 @@ export default function DispensaryFinderPage() {
               {d.city}, {d.state}
             </p>
 
-            {d.hours && (
-              <p className="text-xs text-white/60 mt-1">{d.hours}</p>
-            )}
           </div>
         ))}
 
