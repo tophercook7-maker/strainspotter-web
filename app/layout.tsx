70e4f8c4084f74d67b0b5435e313427be614d680
tophercook7-maker <tophercook7-maker@users.noreply.github.com>
2026-01-11 00:04:19 -0600

    chore: reset globals and simplify layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white min-h-screen">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
